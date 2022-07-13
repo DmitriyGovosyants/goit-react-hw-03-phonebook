9fa6948b491fc09ba234c9597c14b9186881444b
DmitriyGovosyants <Demiangvs@gmail.com>
2022-07-13 16:14:57 +0300

    feat: save contacts to localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,6 +9,21 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = (name, number) => {
     const { contacts } = this.state;
     if (contacts.find(el => el.name === name)) {
